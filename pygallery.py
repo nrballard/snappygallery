500d3053f7d432e1edf2f9e105760173e77a44a8
Nick Ballard <nrballard@gmail.com>
2014-07-15 21:23:49 -0600

    Moved main routine into a function,
    Moved main routine into a function and added a command-line interface

--- pygallery.py
+++ pygallery.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python
 
+from os import path
 import glob
+import sys
 
-tab = "    "
-filelist = []
-filenames = []
-counter = 0
+def generate_gallery(img_dir, gallery_file):
+    tab = "    "
+    file_list = []
+    file_names = []
+  
+    counter = 0
+    for x in glob.glob(img_dir + '*.jpg'):
+        rel_dir = path.join(path.relpath(path.dirname(x), path.dirname(gallery_file)), path.basename(x))
+        file_list.append(rel_dir)
 
-for x in glob.glob("nbnet/imgs/*"):
-    filelist.append(x)
+    for x in file_list:
+        file_names.append(x.split('/')[-1])
 
-for x in filelist:
-    filenames.append(x.split("/")[2])
-
-with open("gallery.html", "w") as outfile:
-    outfile.write("""<html>
+    with open(gallery_file, "w") as outfile:
+        outfile.write("""<html>
 {}<head>
 {}<title>Gallery</title>
 {}<head>
 {}<body>
 {}<table>\n""".format(tab, tab*2, tab, tab, tab*2))
-    for x in range(7):
-        outfile.write(tab*3 + "<tr>\n")
-        for y in range(3):
-            outfile.write(tab*4 + "<td><a href=\"" + filelist[counter] + 
-                "\">" + "<img src=\"" + filelist[counter] + "\"" +
-                " " + "alt=\"" + filenames[counter] + "\" /></a></td>\n")
-            counter += 1
-        outfile.write(tab*3 + "</tr>\n")
-    outfile.write("""{}</table>
+        for x in range(7):
+            outfile.write(tab*3 + "<tr>\n")
+            for y in range(3):
+                outfile.write(tab*4 + "<td><a href=\"javascript:showImg('" + file_list[counter] + 
+                    "')\">" + "<img width=\"240\" height=\"180\" src=\"" + file_list[counter] + "\"" +
+                    " alt=\"" + file_names[counter] + "\" /></a></td>\n")
+                counter += 1
+            outfile.write(tab*3 + "</tr>\n")
+        outfile.write("""{}</table>
 {}</body>
 </html>""".format(tab*2, tab))
+
+if len(sys.argv) < 3:
+    print "Usage: pygallery <IMAGE_DIRECTORY> <GALLERY_FILE>"
+else:
+    img_dir = sys.argv[1]
+    gallery_file = sys.argv[2]
+    generate_gallery(img_dir, gallery_file)
+
 
